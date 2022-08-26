629d24b211dcfea0af623b3030451809a4aa8c15
jinmu333 <dreamhunter2333@gmail.com>
2022-08-27 01:42:25 +0800

    feat: init

--- SakanaWidgetPlugin.vue
+++ SakanaWidgetPlugin.vue
@@ -1,17 +1,38 @@
 <template>
-  <div></div>
+  <div>
+    <div id="sakana-widget"></div>
+    <script>
+      function initSakanaWidget() {
+        SakanaWidget({ defaultCharacter: "takina" });
+      }
+    </script>
+    <script
+      async
+      onload="initSakanaWidget()"
+      src="https://cdn.jsdelivr.net/npm/sakana-widget@1.0.0/lib/sakana.min.js"
+    ></script>
+  </div>
 </template>
 
-<script>
-import SakanaWidget from "sakana-wdiget";
+// <script>
+// import SakanaWidget from "sakana-widget";
 
-export default {
-  name: "SakanaWidgetPlugin",
+// export default {
+//   name: "SakanaWidgetPlugin",
 
-  mounted() {
-    document.addEventListener("DOMContentLoaded", () => {
-      SakanaWidget({ defaultCharacter: "chisato" });
-    });
-  },
-};
+//   mounted() {
+//     window.addEventListener("DOMContentLoaded", () => {
+//       SakanaWidget({ defaultCharacter: "chisato" });
+//     });
+//   },
+// };
+//
 </script>
+
+ <style>
+#sakana-widget {
+  position: fixed;
+  right: 24px;
+  bottom: 24px;
+}
+</style>
