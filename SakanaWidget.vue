dc48f16f58b6b43b856c5a499f5894da3f56d72c
jinmu333 <dreamhunter2333@gmail.com>
2022-08-26 22:55:07 +0800

    feat: init

---
+++ SakanaWidget.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+import SakanaWidget from "sakana-wdiget";
+export default {
+  name: "SakanaWidget",
+
+  mounted() {
+    document.addEventListener("DOMContentLoaded", () => {
+      SakanaWidget({ defaultCharacter: "chisato" });
+    });
+  },
+};
+</script>
---
+++ enhanceAppFile.js
@@ -0,0 +1,6 @@
+import SakanaWidget from './SakanaWidget.vue'
+
+export default ({ Vue }) => {
+  // eslint-disable-next-line vue/match-component-file-name
+  Vue.component('SakanaWidget', SakanaWidget)
+}
